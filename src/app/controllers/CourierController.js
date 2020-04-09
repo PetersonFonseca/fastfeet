776b80cf386587a1d45c0fa92a9db15d29e6dffa
Peterson Fonseca <petersonjf@gmail.com>
2020-04-09 09:30:15 -0300

    desafio 3 em andamento

--- src/app/controllers/CourierController.js
+++ src/app/controllers/CourierController.js
@@ -1,3 +1,52 @@
-class CourierController {}
+import * as Yup from 'yup';
+import Courier from '../models/Courier';
+
+class CourierController {
+  async index(req, res) {
+    const couriers = await Courier.findAll();
+
+    return res.json(couriers);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      avatar_id: Yup.string().required(),
+      email: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
+    const courier = await Courier.create(req.body);
+
+    return res.json(courier);
+  }
+
+  async update(req, res) {
+    const courier = await Courier.findOne({
+      where: { id: req.params.id },
+    });
+
+    if (!courier) {
+      return res.status(400).json({ error: 'Courier alredy exists' });
+    }
+
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      avatar_id: Yup.string().required(),
+      email: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
+    const rec = await courier.update(req.body);
+
+    return res.json(rec);
+  }
+}
 
 export default new CourierController();
--- src/app/models/Courier.js
+++ src/app/models/Courier.js
@@ -1,5 +1,19 @@
 import Sequelize, { Model } from 'sequelize';
 
-class Courier extends Model {}
+class Courier extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        avatar_id: Sequelize.STRING,
+        email: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+}
 
 export default Courier;
